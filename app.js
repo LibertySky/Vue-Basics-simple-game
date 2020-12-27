b2076be02781aecd2522cf69f2fc1f0f99a6b94d
LibertySky <hello@LibertySkygraphics.com>
2020-12-27 16:48:15 -0400

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
 			monsterHealth: 100,
 			currentRound: 0,
 			winner: null,
+			logMessages: [],
 		};
 	},
 	computed: {
@@ -33,16 +34,19 @@ const app = Vue.createApp({
 			this.currentRound++;
 			const attackValue = randomValue(1, 10);
 			this.monsterHealth -= attackValue;
+			this.addLogMessage('player', 'attack', attackValue);
 			this.attackPlayer();
 		},
 		attackPlayer() {
 			const attackValue = randomValue(5, 12);
 			this.playerHealth -= attackValue;
+			this.addLogMessage('monster', 'attack', attackValue);
 		},
 		specialAttackMonster() {
 			this.currentRound++;
 			const attackValue = randomValue(10, 25);
 			this.monsterHealth -= attackValue;
+			this.addLogMessage('player', 'special-attack', attackValue);
 			this.attackPlayer();
 		},
 		healPlayer() {
@@ -52,6 +56,7 @@ const app = Vue.createApp({
 				this.playerHealth = 100;
 			} else {
 				this.playerHealth += healValue;
+				this.addLogMessage('player', 'heal', healValue);
 				this.attackPlayer();
 			}
 		},
@@ -60,6 +65,17 @@ const app = Vue.createApp({
 			this.monsterHealth = 100;
 			this.currentRound = 0;
 			this.winner = null;
+			this.logMessages = [];
+		},
+		surrender() {
+			this.winner = 'player';
+		},
+		addLogMessage(who, what, value) {
+			this.logMessages.unshift({
+				actionBy: who,
+				actionType: what,
+				actionValue: value,
+			});
 		},
 	},
 	watch: {
